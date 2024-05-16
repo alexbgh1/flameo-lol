5c9306b2faa266424ccb3a47b5df44f0447f5d6a
alexbgh1 <82188424+alexbgh1@users.noreply.github.com>
2024-05-15 22:19:55 -0400

    [fix]: trying to fix sound path

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useRef } from "react";
+import { resolvePath } from "../utils";
 
 interface PaginationProps {
   currentIndex: number;
@@ -15,7 +16,7 @@ const Pagination = ({ currentIndex, goPreviousPage, goNextPage, maxIndex }: Pagi
 
   return (
     <>
-      <audio ref={soundRef} src="/sound/message-sound.mp3" />
+      <audio ref={soundRef} src={resolvePath("/sound/message-sound.mp3")} />
       <button
         className="transition-colors duration-300 text-zinc-400 hover:text-zinc-300 disabled:opacity-50 disabled:cursor-not-allowed"
         onClick={goPreviousPage}
