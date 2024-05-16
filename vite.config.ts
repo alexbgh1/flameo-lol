8e8c823c4d4a53800ab030642b54b6cad5584be3
alexbgh1 <alex.garnica@alumnos.uach.cl>
2024-05-15 21:17:39 -0400

    [fix]: vite config deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://alexbgh.github.io/flameo-lol/",
+  base: "/flameo-lol/",
   plugins: [react()],
+  publicDir: "public",
 });
